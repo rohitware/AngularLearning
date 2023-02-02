0f3036c975f8e1fcf03ab03f036e42e11bde29c4
rohitware <rohitware99@gmail.com>
2023-02-02 23:12:49 +0530

    initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular App';
+
+  name = 'Rohit';
+
+  imageUrl = 'https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg';
+
+  d: Date = new Date();
+
+  d1: string = new Date().toLocaleString();
+
+}
