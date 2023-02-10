c1b80298b9d29609f9bd5ea13df78cfef91816ef
rohitware <rohitware99@gmail.com>
2023-02-10 22:26:44 +0530

    Create ngclass.component.ts

---
+++ src/app/ngclass/ngclass.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ngclass',
+  templateUrl: './ngclass.component.html',
+  styleUrls: ['./ngclass.component.css']
+})
+export class NgclassComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  peoples: any[] = [
+    {
+      'name': 'Jack',
+      'country': 'UK'
+    },
+    {
+      'name': 'Sumit',
+      'country': 'USA'
+    },
+    {
+      'name': 'Anup',
+      'country': 'HK'
+    },
+    {
+      'name': 'Harmany',
+      'country': 'UK'
+    },
+    {
+      'name': 'Surya',
+      'country': 'USA'
+    },
+  ]
+}
