7a6d9a06dbba0dd4f6a26249d9c3a8c54f0d6483
rohitware <rohitware99@gmail.com>
2023-02-15 18:50:28 +0530

    Create tdf.component.ts

---
+++ src/app/tdf/tdf.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-tdf',
+  templateUrl: './tdf.component.html',
+  styleUrls: ['./tdf.component.css']
+})
+export class TDFComponent {
+
+  login(form: NgForm) {
+    console.log(form.value)
+      }
+}
+
