3d38496a4ccc3fa8842a30716154a387ac8cb557
rohitware <rohitware99@gmail.com>
2023-03-11 22:48:21 +0530

    update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,7 +9,7 @@ import { MyserviceService } from '../Services/myservice.service';
 })
 export class HomeComponent implements OnInit {
 
-  name: string = '';
+  namesearch: string = '';
 
   products = {};
   public users: any;
@@ -38,7 +38,7 @@ export class HomeComponent implements OnInit {
     }
   ]
 
-  // name = 'Rohit';
+  name = 'Rohit';
 
   imageUrl = 'https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg';
 
