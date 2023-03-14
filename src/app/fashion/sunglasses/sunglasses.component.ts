4b2bc97dc0140c662d04fbf7632a0829b9f35901
rohitware <rohitware99@gmail.com>
2023-03-14 21:25:35 +0530

    Create sunglasses.component.ts

---
+++ src/app/fashion/sunglasses/sunglasses.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sunglasses',
+  templateUrl: './sunglasses.component.html',
+  styleUrls: ['./sunglasses.component.css']
+})
+export class SunglassesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
