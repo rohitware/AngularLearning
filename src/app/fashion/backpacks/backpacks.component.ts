ab38b29cde5b2baf4c24983724bf30b40131940b
rohitware <rohitware99@gmail.com>
2023-03-14 21:24:56 +0530

    Create backpacks.component.ts

---
+++ src/app/fashion/backpacks/backpacks.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-backpacks',
+  templateUrl: './backpacks.component.html',
+  styleUrls: ['./backpacks.component.css']
+})
+export class BackpacksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
