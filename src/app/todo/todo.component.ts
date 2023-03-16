d592857f0a62c85622248841a1d7efa73f5a085a
rohitware <rohitware99@gmail.com>
2023-03-16 22:57:43 +0530

    ToDo CRUD
    ToDo CRUD

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../services/todo.service';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+
+  todoList: any = [];
+  // DI 
+  constructor(private todoService: TodoService) { }
+
+  ngOnInit() {
+    this.listToDos();
+  }
+
+  createTodo() {
+    let todo = {
+      id: new Date().getTime(),
+      title: `Format the data of firebase`
+    }
+
+    this.todoService.create(todo).subscribe((response) => {
+      console.log('todo create', response);
+      this.listToDos();
+    }, (error => {
+      console.log(error)
+    }))
+  }
+
+  listToDos() {
+    this.todoService.list().subscribe((res) => {
+      console.log(res)
+      this.todoList = res;
+    }, (error => {
+      console.log(error);
+    }))
+  }
+
+  editTodo(todo: any) {
+    let editData = {
+      id: new Date().getTime(),
+      title: `Edited title`
+    }
+
+    this.todoService.update(todo.id, editData).subscribe(res => {
+      this.listToDos();
+    })
+  }
+
+  deleteTodo(id: any) {
+    this.todoService.delete(id).subscribe(res => {
+      console.log('Record has been deleted');
+      this.listToDos();
+    })
+  }
+}
