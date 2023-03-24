308004b100932d4ae5853bd8a17fc5c0fe62f46c
rohitware <rohitware99@gmail.com>
2023-03-24 17:46:10 +0530

    Rxjs operators: take, takeLast, takeUntil
    Rxjs operators: take, takeLast, takeUntil

--- src/app/rxjs/rxjs.component.ts
+++ src/app/rxjs/rxjs.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { from, of } from 'rxjs';
+import { from, interval, of, timer } from 'rxjs';
+import { take, takeLast, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rxjs',
@@ -13,18 +14,44 @@ export class RxjsComponent implements OnInit {
   ngOnInit() {
 
     // of and from operators
-    const ofObs = of([1,2,3]);
+    const ofObs = of([1, 2, 3]);
 
-    ofObs.subscribe(res=>{
-      console.log('of example', res);      
+    ofObs.subscribe(res => {
+      console.log('of example', res);
     });
 
-    const fromObs = from([1,2,3]);
+    const fromObs = from([1, 2, 3]);
 
-    fromObs.subscribe(res=>{
+    fromObs.subscribe(res => {
       console.log(`from example`, res);
-      
+    });
+
+
+    // Take operator 
+    const source = interval(1000);
+    source.pipe(
+      take(5)
+    ).subscribe(res => {
+      console.log('take operator', res)
+    })
+
+    // take last 
+    let randomNames = ['Rohit', 'Codemind', 'Angular', 'HTML', 'Javascript', 'Typescript'];
+    const results = from(randomNames);
+    results.pipe(
+      takeLast(2)
+    ).subscribe(res => {
+      console.log('takeLast example', res);
+    })
+
+    // takeUntil 
+    const custObs = interval(1000);
+    let condition = timer(6000);
+    custObs.pipe(
+      takeUntil(condition)
+    ).subscribe(res => {
+      console.log('takeUntil', res)
     })
   }
-
 }
+
